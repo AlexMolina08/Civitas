9e23c8028cd7abf98391cd7e44df5d806b33eec9
Alex Molina <alex98criado@gmail.com>
2020-10-03 16:47:18 +0200

    añadida carpeta ruby

--- CivitasRuby/lib/respuestas.rb
+++
@@ -1,8 +0,0 @@
-module Civitas
-  module Respuestas
-    SI = :si
-    NO = :no
-  end
-  
-  respuestas = [Respuestas::NO , Respuestas::SI]
-end
--- CivitasRuby/lib/salidas_carcel.rb
+++
@@ -1,8 +0,0 @@
-module Civitas
-  
-  module Salidas_Carcel
-    PAGANDO = :pagando
-    TIRANDO = :tirando
-  end
-  salidas_carcel = [Salidas_Carcel::PAGANDO , Salidas_Carcel::TIRANDO]
-end
